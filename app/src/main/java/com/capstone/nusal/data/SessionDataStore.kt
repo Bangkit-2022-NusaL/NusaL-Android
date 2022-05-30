4a78d92d9e2b22074d9ed4066bc5e42fe903f29e
Kenrico Valens <kenrico.valens@ui.ac.id>
2022-05-30 22:47:29 +0700

    MainActivity UI improvement

--- app/src/main/java/com/capstone/nusal/data/SessionDataStore.kt
+++ app/src/main/java/com/capstone/nusal/data/SessionDataStore.kt
@@ -38,6 +38,7 @@ class SessionDataStore private constructor(private val dataStore: DataStore<Pref
     companion object {
         val IS_LOGIN_KEY = booleanPreferencesKey("is_login")
         val TOKEN_KEY = stringPreferencesKey("user_token")
+        val NAME_KEY = stringPreferencesKey("user_name")
 
         @Volatile
         private var INSTANCE: SessionDataStore? = null
